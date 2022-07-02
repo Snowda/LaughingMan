8b4b7925d557ee942b8e3ce5e159788b425c9d9f
Snowda <conor@dusklab.com>
2022-07-02 17:01:44 -0400

    Initializing rust version of project

---
+++ src/laugh.rs
@@ -0,0 +1,11 @@
+
+#[tokio::main]
+async fn main() {
+	// TODO load Animated SVG
+	// TODO load a webcam or video
+	// TODO take command line arguments
+	// TODO face detection
+	// TODO face tracking
+	// TODO save screenshots key binding
+	// TODO calculate / display FPS (optional)
+}
